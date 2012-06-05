bb3d623cc68ac00617770d0d5a315bf8105f60c6
vasile.giorgi@gmail.com <vasile.giorgi@gmail.com@37a09a1e-db6a-e8aa-a2b3-0bc1178e615e>
2012-06-05 16:18:47 +0000

    1st commit

---
+++ cache/pages_extra.php
@@ -0,0 +1,31 @@
+<?php
+a::$arSiteMap = array (
+  'i1' => 
+  array (
+    'name' => 'admin', //onyx?
+    'parent' => '',
+    'default' => true,
+    'title' => 'Agate Web Framework - Web project Administration',
+	'security' => 'BC747117-7BB4-498A-BB3F-8EE7D25CE65D'
+  ),
+  'i2' => 
+  array (
+    'name' => 'login', //including register form
+    'parent' => '',
+  ),
+  'i3' => 
+  array (
+    'name' => 'recover', //account
+    'parent' => '',
+  ),
+    array (
+    'name' => 'auth-ajax', //authenticate using ajax call
+    'parent' => '',
+  ),
+  'i6' => 
+  array (
+    'name' => 'section',
+    'parent' => '',
+  )
+);
+?>
